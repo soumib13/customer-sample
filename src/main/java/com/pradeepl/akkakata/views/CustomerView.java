6592430a069b73a199c944f3744936ca509772b5
Pradeep Loganathan <3273111+PradeepLoganathan@users.noreply.github.com>
2026-06-08 18:19:28 +1000

    Adding view functionality 1. Added a table updater. 2. Implemented handlers for 2 events in the table updater. 3. Created a get endpoint to use the view for query
    Currently not working fine.

--- src/main/java/com/pradeepl/akkakata/views/CustomerView.java
+++ src/main/java/com/pradeepl/akkakata/views/CustomerView.java
@@ -1,15 +1,40 @@
-// package com.pradeepl.akkakata.views;
+package com.pradeepl.akkakata.views;
 
-// import akka.javasdk.annotations.ComponentId;
-// import akka.javasdk.annotations.Query;
-// import akka.javasdk.view.View;
+import java.util.List;
 
-// @ComponentId("customerview")
-// public class CustomerView extends View{
+import com.pradeepl.akkakata.domain.entities.CustomerEntity;
+import com.pradeepl.akkakata.domain.events.CustomerEvents.customerCreated;
+import com.pradeepl.akkakata.domain.events.CustomerEvents.customerDeleted;
 
-//     @Query("SELECT * AS entries FROM application_queue WHERE status = :status")
-//     public QueryEffect<> getByStatus(String status) {
-//         return queryResult();
-//     }
-    
-// }
+import akka.javasdk.annotations.ComponentId;
+import akka.javasdk.annotations.Consume;
+import akka.javasdk.annotations.Query;
+import akka.javasdk.annotations.Table;
+import akka.javasdk.view.TableUpdater;
+import akka.javasdk.view.View;
+
+@ComponentId("customerview")
+public class CustomerView extends View{
+
+    public record customerEntry(String customerId, String FirstName, String LastName, boolean deleted){}
+    public record customerEntries(List<customerEntry> entries) {}
+
+    @Query("SELECT * FROM customers_table")
+    public QueryEffect<customerEntry> getAll() {
+         return queryResult();
+    }
+
+    @Table("customers_table")
+     @Consume.FromEventSourcedEntity(CustomerEntity.class)
+    public static class CustomersUpdater extends TableUpdater<customerEntry> {
+        
+        public Effect<customerEntry> onEvent(customerCreated event) {
+            return effects().updateRow(new customerEntry(event.customerId(), event.FirstName(),event.LastName(), false));
+        }
+
+        public Effect<customerEntry> onEvent(customerDeleted event) {
+            return effects().updateRow(new customerEntry(event.customerId(), event.FirstName(),event.LastName(), true));
+        }
+
+    }
+}
